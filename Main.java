1949abe0510076345587749a123985b6a80d9b28
TramVo276 <tvo941352@gmail.com>
2024-11-05 03:43:31 +0000

    ihweeoi

--- Main.java
+++ Main.java
@@ -4,6 +4,16 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner = new Scanner(System.in);
+    System.out.println("Please enter a double: ");
+    int value1 = scanner.nextLine();
+    if (value1 == 12.345);
+    {
+      System.out.println("YES");
+    }
+    else
+    {
+      System.out.println("NO");
+    }
   }
 }
